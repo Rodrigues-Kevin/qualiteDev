2a20eca7f407c7ae0c90c9879884034a93e6aba5
RODRIGUES Kevin <12405660@q10420.iutv.univ-paris13.fr>
2025-05-23 11:18:52 +0200

    Création du programme de test de la classe Calculatrice

---
+++ CalculatriceTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatriceTest {
+    Calculatrice calculatrice;
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addTest() {
+        assertEquals(2, calculatrice.add(1, 1), "1 + 1 = 2");
+    }
+
+    @Test
+    @DisplayName("1 / 2 = 0.5")
+    void divTest() {
+        assertEquals(0.5, calculatrice.div(1, 2), "1 / 2 = 0.5");
+    }
+
+    @Test
+    @DisplayName("2 * 2 = 4")
+    void multTest() {
+        assertEquals(4, calculatrice.mult(2, 2), "2 * 2 = 4");
+    }
+}
